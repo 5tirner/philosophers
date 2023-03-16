cefa3072809d683d638b8f06a17f3b9bbb3679fc
zasabri <zasabri@gmail.com>
2023-03-16 17:40:02 +0100

    @5tirner

--- philosopher.c
+++ philosopher.c
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:20:55 by zasabri           #+#    #+#             */
-/*   Updated: 2023/03/16 16:17:08 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/03/16 17:39:52 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ void	eating(t_time *times)
 	int	j;
 	
 	j = 0;
+	//printf("befor\n");
 	printf("philo %d start eating in the time %lu\n", times->i, x);
+	//printf("here1\n");
 	while (j < times->time_to_eat)
 	{
 		pthread_mutex_lock(&times->mutex);
 		j++;
 		x++;
-		pthread_mutex_destroy(&times->mutex);
+		pthread_mutex_unlock(&times->mutex);
 	}
 	printf("philo %d ends in the time: %lu\n", times->i, x);
 }
@@ -41,7 +43,7 @@ void	sleeping(t_time *times)
 		pthread_mutex_lock(&times->mutex);
 		j++;
 		x++;
-		pthread_mutex_destroy(&times->mutex);
+		pthread_mutex_unlock(&times->mutex);
 	}
 	printf("philo %d weak uo at time: %lu\n", times->i, x);
 }
@@ -51,8 +53,11 @@ void	*cyrcle(void *times)
 	t_time	*time;
 	
 	time = (t_time *)times;
+	pthread_mutex_init(&time->mutex, NULL);
+	printf("Philo %d takes a fork\n", time->i);
 	eating(time);
 	sleeping(time);
+	pthread_mutex_destroy(&time->mutex);
 	return (0);
 }
 
@@ -71,8 +76,6 @@ int main(int ac, char **av)
 		
 		while (times.i <= philo_nbr)
 		{
-			pthread_mutex_init(&times.mutex, NULL);
-			printf("Philo %d takes a fork\n", times.i);
 			if (pthread_create(&philo[times.i], NULL, &cyrcle, &times))
 			{
 				printf("Thread created is failed\n");
@@ -83,7 +86,6 @@ int main(int ac, char **av)
 				printf("Somthing unexpected is happen\n");
 				return (1);
 			}
-			pthread_mutex_destroy(&times.mutex);
 			times.i++;
 		}
 	}
