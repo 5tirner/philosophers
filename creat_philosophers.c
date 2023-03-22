a610e15df374d6e10b538ca711c059c65c700a45
zasabri <zasabri@gmail.com>
2023-03-22 10:04:03 +0000

    @5tirner

--- creat_philosophers.c
+++ creat_philosophers.c
@@ -1,33 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.h                                      :+:      :+:    :+:   */
+/*   creat_philosophers.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/28 13:23:23 by zasabri           #+#    #+#             */
-/*   Updated: 2023/03/22 09:54:53 by zasabri          ###   ########.fr       */
+/*   Created: 2023/03/22 10:00:59 by zasabri           #+#    #+#             */
+/*   Updated: 2023/03/22 10:02:03 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHER_H
-# define PHILOSOPHER_H
+#include "philosopher.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <pthread.h>
-
-typedef struct s_infos
-{
-	int	philo_nbr;
-	int	to_eat;
-	int	to_die;
-	int	to_sleep;
-	int	repeat_eat;
-	int	timer;
-}	t_infos;
-
-int		ft_atoi(const char *str);
-
-#endif
--- philosopher.c
+++ philosopher.c
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:20:55 by zasabri           #+#    #+#             */
-/*   Updated: 2023/03/22 09:51:30 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/03/22 09:56:52 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	init_times(int ac, char **av, t_infos *infos)
 	if (ac == 6)
 		infos->repeat_eat = ft_atoi(av[5]);
 }
+
 int	check_that_arg(char *av)
 {
 	int	i;
@@ -74,10 +75,10 @@ int	check_input(t_infos *info, char **av)
 	return (0);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_infos	infos;
-	
+
 	if (ac == 5 || ac == 6)
 	{
 		init_times(ac, av, &infos);
@@ -85,5 +86,8 @@ int main(int ac, char **av)
 			return (1);
 	}
 	else
+	{
 		write(2, "Invalid Number Of Argements\n", 28);
+		return (1);
+	}
 }
