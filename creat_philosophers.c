6b66269f184d4417ad924c51a92ef5678fb60b8c
zasabri <zasabri@gmail.com>
2023-03-22 12:59:20 +0000

    @5tirner

--- creat_philosophers.c
+++ creat_philosophers.c
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 10:00:59 by zasabri           #+#    #+#             */
-/*   Updated: 2023/03/22 12:58:41 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/03/22 12:59:18 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	creat_philosophers(t_infos *info)
 		info->philo[i].left = i;
 		info->philo[i].right = (i + 1) % ph_nb;
 		info->philo[i].last_meal = 0;
-		info->philo[i].info = info;
+		//info->philo[i].info = info;
 		i--;
 	}
 	// i = info->philo_nbr - 1;
