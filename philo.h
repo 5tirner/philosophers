32809f29a8367423368ec3a2dd299c5659daab13
Zakaria Sabri <zasabri@e2r2p7.1337.ma>
2023-04-12 21:57:29 +0000

    @5tirner

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 19:32:38 by zasabri           #+#    #+#             */
-/*   Updated: 2023/04/07 00:25:18 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/04/12 21:46:30 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,14 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-typedef struct s_philos
-{
-	int				right;
-	int				left;
-	int				pos;
-	int				last_meal;
-	struct	s_infos	*infos;
-	
-}	t_philos;
-
 typedef struct s_infos
 {
-	unsigned long	ph_nb;
+	int				ph_nb;
 	unsigned long	eat;
 	unsigned long	sleep;
 	unsigned long	die;
 	unsigned long	m_nbr;
-	t_philos		*philo;
 	pthread_mutex_t	*forks;
-	pthread_mutex_t	meals;
-	pthread_mutex_t	rdwr;
 }	t_infos;
 
 unsigned long	ft_atoi(const char *str);
